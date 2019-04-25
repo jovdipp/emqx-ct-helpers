e18647b40b5d47cf346c92aee43cddb41c4bf3ed
Gilbert <gilbertw@emqx.io>
2019-04-25 11:45:16 +0800

    Move API functions of emqx_ct_helpers module from emqx here (#11)
    * Move API functions of emqx_ct_helpers module from emqx here
    
    * Add change_emqx_opts api

--- src/emqx_ct_helpers.erl
+++ src/emqx_ct_helpers.erl
@@ -27,7 +27,51 @@
         , stop_apps/1
         , reload/2
         , deps_path/2
+        , flush/0
+        , flush/1
         ]).
+
+-export([ ensure_mnesia_stopped/0
+        , wait_for/4
+        , change_emqx_opts/1
+        , client_ssl_twoway/0
+        , client_ssl/0
+        , wait_mqtt_payload/1
+        , not_wait_mqtt_payload/1]).
+
+-define(CIPHERS, [{ciphers,
+                   ["ECDHE-ECDSA-AES256-GCM-SHA384",
+                    "ECDHE-RSA-AES256-GCM-SHA384",
+                    "ECDHE-ECDSA-AES256-SHA384",
+                    "ECDHE-RSA-AES256-SHA384","ECDHE-ECDSA-DES-CBC3-SHA",
+                    "ECDH-ECDSA-AES256-GCM-SHA384",
+                    "ECDH-RSA-AES256-GCM-SHA384",
+                    "ECDH-ECDSA-AES256-SHA384","ECDH-RSA-AES256-SHA384",
+                    "DHE-DSS-AES256-GCM-SHA384","DHE-DSS-AES256-SHA256",
+                    "AES256-GCM-SHA384","AES256-SHA256",
+                    "ECDHE-ECDSA-AES128-GCM-SHA256",
+                    "ECDHE-RSA-AES128-GCM-SHA256",
+                    "ECDHE-ECDSA-AES128-SHA256",
+                    "ECDHE-RSA-AES128-SHA256",
+                    "ECDH-ECDSA-AES128-GCM-SHA256",
+                    "ECDH-RSA-AES128-GCM-SHA256",
+                    "ECDH-ECDSA-AES128-SHA256","ECDH-RSA-AES128-SHA256",
+                    "DHE-DSS-AES128-GCM-SHA256","DHE-DSS-AES128-SHA256",
+                    "AES128-GCM-SHA256","AES128-SHA256",
+                    "ECDHE-ECDSA-AES256-SHA","ECDHE-RSA-AES256-SHA",
+                    "DHE-DSS-AES256-SHA","ECDH-ECDSA-AES256-SHA",
+                    "ECDH-RSA-AES256-SHA","AES256-SHA",
+                    "ECDHE-ECDSA-AES128-SHA","ECDHE-RSA-AES128-SHA",
+                    "DHE-DSS-AES128-SHA","ECDH-ECDSA-AES128-SHA",
+                    "ECDH-RSA-AES128-SHA","AES128-SHA"]}]).
+
+-define(MQTT_SSL_TWOWAY, [{cacertfile, filename:join(["etc", "certs", "cacert.pem"])},
+                          {verify, verify_peer},
+                          {fail_if_no_peer_cert, true}]).
+
+-define(MQTT_SSL_CLIENT, [{keyfile, filename:join(["etc", "certs", "client-key.pem"])},
+                          {cacertfile, filename:join(["etc", "certs", "cacert.pem"])},
+                          {certfile, filename:join(["etc", "certs", "client-cert.pem"])}]).
 
 %%------------------------------------------------------------------------------
 %% APIs
@@ -86,3 +130,116 @@ reload(App, SpecAppConfigHandler) ->
     start_app(App, SpecAppConfigHandler),
     application:start(App).
 
+ensure_mnesia_stopped() ->
+    ekka_mnesia:ensure_stopped(),
+    ekka_mnesia:delete_schema().
+
+%% Help function to wait for Fun to yield 'true'.
+wait_for(Fn, Ln, F, Timeout) ->
+    {Pid, Mref} = erlang:spawn_monitor(fun() -> wait_loop(F, catch_call(F)) end),
+    wait_for_down(Fn, Ln, Timeout, Pid, Mref, false).
+
+change_emqx_opts(SslType) ->
+    {ok, Listeners} = application:get_env(emqx, listeners),
+    GenNewListener = fun({Protocol, Port, Opts} = Listener, Acc) ->
+                         case Protocol of
+                             ssl ->
+                                 SslOpts = proplists:get_value(ssl_options, Opts),
+                                 Keyfile = deps_path(emqx, filename:join(["etc", "certs", "key.pem"])),
+                                 Certfile = deps_path(emqx, filename:join(["etc", "certs", "cert.pem"])),
+                                 TupleList1 = lists:keyreplace(keyfile, 1, SslOpts, {keyfile, Keyfile}),
+                                 TupleList2 = lists:keyreplace(certfile, 1, TupleList1, {certfile, Certfile}),
+                                 TupleList3 =
+                                     case SslType of
+                                         ssl_twoway ->
+                                             CAfile = deps_path(emqx, proplists:get_value(cacertfile, ?MQTT_SSL_TWOWAY)),
+                                             MutSslList = lists:keyreplace(cacertfile, 1, ?MQTT_SSL_TWOWAY, {cacertfile, CAfile}),
+                                             lists:merge(TupleList2, MutSslList);
+                                         _ ->
+                                             lists:filter(fun ({cacertfile, _}) -> false;
+                                                              ({verify, _}) -> false;
+                                                              ({fail_if_no_peer_cert, _}) -> false;
+                                                              (_) -> true
+                                                          end, TupleList2)
+                                     end,
+                                 [{Protocol, Port, lists:keyreplace(ssl_options, 1, Opts, {ssl_options, TupleList3})} | Acc];
+                             _ ->
+                                 [Listener | Acc]
+                         end
+                     end,
+    NewListeners = lists:foldl(GenNewListener, [], Listeners),
+    application:set_env(emqx, listeners, NewListeners).
+
+flush() ->
+    flush([]).
+
+flush(Msgs) ->
+    receive
+        M -> flush([M|Msgs])
+    after
+        0 -> lists:reverse(Msgs)
+    end.
+
+client_ssl_twoway() ->
+    [{Key, deps_path(emqx, FilePath)} || {Key, FilePath} <- ?MQTT_SSL_CLIENT] ++ ?CIPHERS.
+
+client_ssl() ->
+    ?CIPHERS ++ [{reuse_sessions, true}].
+
+wait_mqtt_payload(Payload) ->
+    receive
+        {publish, #{payload := Payload}} ->
+            ct:pal("OK - received msg: ~p~n", [Payload])
+    after 1000 ->
+        ct:fail({timeout, Payload, {msg_box, flush()}})
+    end.
+
+not_wait_mqtt_payload(Payload) ->
+    receive
+        {publish, #{payload := Payload}} ->
+            ct:fail({received, Payload})
+    after 1000 ->
+        ct:pal("OK - msg ~p is not received", [Payload])
+    end.
+
+wait_for_down(Fn, Ln, Timeout, Pid, Mref, Kill) ->
+    receive
+        {'DOWN', Mref, process, Pid, normal} ->
+            ok;
+        {'DOWN', Mref, process, Pid, {unexpected, Result}} ->
+            erlang:error({unexpected, Fn, Ln, Result});
+        {'DOWN', Mref, process, Pid, {crashed, {C, E, S}}} ->
+            erlang:raise(C, {Fn, Ln, E}, S)
+    after
+        Timeout ->
+            case Kill of
+                true ->
+                    erlang:demonitor(Mref, [flush]),
+                    erlang:exit(Pid, kill),
+                    erlang:error({Fn, Ln, timeout});
+                false ->
+                    Pid ! stop,
+                    wait_for_down(Fn, Ln, Timeout, Pid, Mref, true)
+            end
+    end.
+
+wait_loop(_F, ok) -> exit(normal);
+wait_loop(F, LastRes) ->
+    receive
+        stop -> erlang:exit(LastRes)
+    after
+        100 ->
+            Res = catch_call(F),
+            wait_loop(F, Res)
+    end.
+
+catch_call(F) ->
+    try
+        case F() of
+            true -> ok;
+            Other -> {unexpected, Other}
+        end
+    catch
+        C : E : S ->
+            {crashed, {C, E, S}}
+    end.
