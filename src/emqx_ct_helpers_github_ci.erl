4d49e0c362669d5345b7c98f95431b55eb1c1cd8
Jóvan S. Dippenaar <jovan@twenty8.enterprises>
2021-04-05 11:07:36 +1200

    fix(github ci): returns matrix wrapped in "include"

--- src/emqx_ct_helpers_github_ci.erl
+++ src/emqx_ct_helpers_github_ci.erl
@@ -40,11 +40,15 @@ ensure_code_paths() ->
     code:add_pathsa(Paths).
 
 get_matrix(SuiteStr) ->
+    SuiteJobsJsonStruts = suite_matrix(SuiteStr),
+    MatrixJsonStruts = {[{<<"include">>,SuiteJobsJsonStruts}]},
+    jiffy:encode(MatrixJsonStruts).
+
+suite_matrix(SuiteStr) ->
     Suite = list_to_atom(SuiteStr),
     Matrix = Suite:job_matrix(),
     Jobs = emqx_ct_helpers_jobs:matrix_jobs(Matrix),
-    JsonJobs = jobs_to_json_structs(Jobs),
-    jiffy:encode(JsonJobs).
+    jobs_to_json_structs(Jobs).
 
 jobs_to_json_structs(Jobs) ->
     [ job_to_struct(Job) || Job <- Jobs ].
@@ -66,16 +70,16 @@ to_binary(Int) -> integer_to_binary(Int).
 -include_lib("eunit/include/eunit.hrl").
 
 matrix_as_json_str_test() ->
-    JsonStr = <<"[{\"name\":\"mysqlV8_tls_ipv4\",\"vectors\":[\"mysqlV8\",\"tls\",\"ipv4\"],",
-                "\"index\":0},{\"name\":\"mysqlV8_tls_ipv6\",\"vectors\":[\"mysqlV8\",\"tls\",",
-                "\"ipv6\"],\"index\":1},{\"name\":\"mysqlV8_tcp_ipv4\",\"vectors\":[\"mysqlV8\",",
-                "\"tcp\",\"ipv4\"],\"index\":2},{\"name\":\"mysqlV8_tcp_ipv6\",\"vectors\":",
-                "[\"mysqlV8\",\"tcp\",\"ipv6\"],\"index\":3},{\"name\":\"mysqlV5_7_tls_ipv4\",",
-                "\"vectors\":[\"mysqlV5_7\",\"tls\",\"ipv4\"],\"index\":4},{\"name\":",
-                "\"mysqlV5_7_tls_ipv6\",\"vectors\":[\"mysqlV5_7\",\"tls\",\"ipv6\"],",
+    JsonStr = <<"{\"include\":[{\"name\":\"mysqlV8_tls_ipv4\",\"vectors\":[\"mysqlV8\",\"tls\",",
+                "\"ipv4\"],\"index\":0},{\"name\":\"mysqlV8_tls_ipv6\",\"vectors\":[\"mysqlV8\",",
+                "\"tls\",\"ipv6\"],\"index\":1},{\"name\":\"mysqlV8_tcp_ipv4\",\"vectors\":",
+                "[\"mysqlV8\",\"tcp\",\"ipv4\"],\"index\":2},{\"name\":\"mysqlV8_tcp_ipv6\",",
+                "\"vectors\":[\"mysqlV8\",\"tcp\",\"ipv6\"],\"index\":3},{\"name\":",
+                "\"mysqlV5_7_tls_ipv4\",\"vectors\":[\"mysqlV5_7\",\"tls\",\"ipv4\"],\"index\":4},",
+                "{\"name\":\"mysqlV5_7_tls_ipv6\",\"vectors\":[\"mysqlV5_7\",\"tls\",\"ipv6\"],",
                 "\"index\":5},{\"name\":\"mysqlV5_7_tcp_ipv4\",\"vectors\":[\"mysqlV5_7\",\"tcp\",",
                 "\"ipv4\"],\"index\":6},{\"name\":\"mysqlV5_7_tcp_ipv6\",\"vectors\":",
-                "[\"mysqlV5_7\",\"tcp\",\"ipv6\"],\"index\":7}]">>,
+                "[\"mysqlV5_7\",\"tcp\",\"ipv6\"],\"index\":7}]}">>,
     ?assertEqual(JsonStr, get_matrix("emqx_jobs_SUITE")).
 
 -endif.
